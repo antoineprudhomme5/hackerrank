756acabb228f6a5cd42f946f36b99cabeb4eb629
prudywsh <antoineprudhomme5@gmail.com>
2017-02-22 23:09:41 +0100

    3 tests case ..

--- contests/week29/a_circle_and_a_square.py
+++ contests/week29/a_circle_and_a_square.py
@@ -4,6 +4,9 @@ def euclidian_dist(a_x, a_y, b_x, b_y):
     return math.sqrt(math.pow(a_x - b_x, 2) + math.pow(a_y - b_y, 2))
 
 def is_in_circle(p_x, p_y, c_x, c_y, r):
+    #if (p_x == 4 and p_y == 10) or (p_x == 5 and p_y == 10):
+        #print("%d %d : %.6f => %.6f" % (p_x, p_y, math.sqrt(math.pow(p_x - c_x, 2) + math.pow(p_y - c_y, 2)), r))
+        #print(math.sqrt(math.pow(p_x - c_x, 2) + math.pow(p_y - c_y, 2)) <= r)
     return math.sqrt(math.pow(p_x - c_x, 2) + math.pow(p_y - c_y, 2)) <= r
 
 def triangle_area(a_x, a_y, b_x, b_y, c_x, c_y):
@@ -12,7 +15,7 @@ def triangle_area(a_x, a_y, b_x, b_y, c_x, c_y):
     c = euclidian_dist(c_x, c_y, a_x, a_y)
     p = (a + b + c) / 2
     temp = p * (p-a) * (p-b) * (p-c)
-    #if (b_x == 13 and b_y == 12) or (b_x == 11 and b_y == 12):
+    #if (b_x == 4 and b_y == 10) or (b_x == 5 and b_y == 10):
         #print("%d => %d : %d %d %d" % (p, temp, a, b, c))
     if temp < 0:
         return 0
@@ -24,8 +27,8 @@ def is_in_square(p_x, p_y, a_x, a_y, b_x, b_y, c_x, c_y, d_x, d_y):
                 triangle_area(b_x, b_y, p_x, p_y, c_x, c_y) + \
                 triangle_area(c_x, c_y, p_x, p_y, d_x, d_y) + \
                 triangle_area(d_x, d_y, p_x, p_y, a_x, a_y)
-    #if (p_x == 13 and p_y == 12) or (p_x == 11 and p_y == 12):
-        #print(areas_sum)
+    #if p_x == 4 and p_y == 10:
+        #print("%d %d : %d" % (p_x, p_y, int(areas_sum)))
     # if the triangles areas sum is greater than the square area, it's outside
     return int(areas_sum) <= SQUARE_AREA
 
@@ -36,11 +39,19 @@ x1, y1, x3, y3 = list(map(int, input().split()))
 half_diagonal = math.floor(euclidian_dist(x1, y1, x3, y3) / 2)
 
 if x3 > x1:
-    x2, y2 = x1 + half_diagonal, y3 - half_diagonal
-    x4, y4 = x3 - half_diagonal, y1 + half_diagonal
+    if y3 > y1:
+        x2, y2 = x1 + half_diagonal, y3 - half_diagonal
+        x4, y4 = x3 - half_diagonal, y1 + half_diagonal
+    else:
+        x2, y2 = x1 + half_diagonal, y3 + half_diagonal
+        x4, y4 = x3 - half_diagonal, y1 - half_diagonal
 else:
-    x4, y4 = x3 + half_diagonal, y1 - half_diagonal
-    x2, y2 = x1 - half_diagonal, y3 + half_diagonal
+    if y3 > y1:
+        x4, y4 = x3 + half_diagonal, y1 - half_diagonal
+        x2, y2 = x1 - half_diagonal, y3 + half_diagonal
+    else:
+        x4, y4 = x3 + half_diagonal, y1 + half_diagonal
+        x2, y2 = x1 - half_diagonal, y3 - half_diagonal
 
 SQUARE_AREA = (x1-x2)**2 + (y1-y2)**2
 

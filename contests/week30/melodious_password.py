14fe17c9dd5298afb389457714f471538ae9f874
prudywsh <antoineprudhomme5@gmail.com>
2017-03-15 10:29:53 +0100

    find all password of size n, alternating vowels and consonants : done

--- contests/week30/melodious_password.py
+++ contests/week30/melodious_password.py
@@ -1,41 +1,40 @@
 vowels = "aeiou"
 consonants = "zrtpmlkjhgfdsqwxcvbn"
 
-end_with_vowel = list(vowels)
-end_with_consonant = list(consonants)
-
 password_length = int(input())
+elements = list(vowels) + list(consonants)
 
-while password_length > 1:
-
-    new_end_with_vowel = []
-    new_end_with_consonant = []
-
-    if password_length > 1 and password_length % 2 == 1:
-
-        for v in vowels:
-            for i in range(len(end_with_consonant)):
-                new_end_with_vowel.append(end_with_consonant[i] + v)
-                new_end_with_consonant.append(v + end_with_consonant[i])
-        
-        for c in consonants:
-            for i in range(len(end_with_vowel)):
-                new_end_with_consonant.append(end_with_vowel[i] + c)
-                new_end_with_vowel.append(c + end_with_vowel[i])
-
-    else :
-
-        for v in vowels:
-            for i in range(len(end_with_consonant)):
-                new_end_with_vowel.append(end_with_consonant[i] + v)
-        
-        for c in consonants:
-            for i in range(len(end_with_vowel)):
-                new_end_with_consonant.append(end_with_vowel[i] + c)
-
-    end_with_consonant = new_end_with_consonant
-    end_with_vowel = new_end_with_consonant
-    password_length -= 1
-
-all = set(end_with_consonant + end_with_vowel)
-[print(w) for w in all]
+if password_length == 1:
+    [print(v) for v in elements]    
+else:
+    i = 1
+    while password_length > i:
+        new_elements = []
+        if i == 1:
+            for e in elements:
+                if e in vowels:
+                    for c in consonants:
+                        new_elements.append(e + c)
+                else:
+                    for v in vowels:
+                        new_elements.append(e + v)
+        else:
+            for e in elements:
+                # check first char
+                if e[0] in vowels:
+                    for c in consonants:
+                        new_elements.append(c + e)
+                else:
+                    for v in vowels:
+                        new_elements.append(v + e)
+                # check last char
+                if e[-1] in vowels:
+                    for c in consonants:
+                        new_elements.append(e + c)
+                else:
+                    for v in vowels:
+                        new_elements.append(e + v)
+        elements = new_elements
+        i += 1
+    
+    [print(e) for e in set(elements)]

81ec0d6b27b00e36b457a3c22754b9819478c654
prudywsh <antoineprudhomme5@gmail.com>
2017-08-14 21:48:33 +0200

    forgot to add some changes

--- cracking_the_coding_interview/ctci-balanced-brackets.py
+++ cracking_the_coding_interview/ctci-balanced-brackets.py
@@ -5,10 +5,10 @@ def are_balanced_brackets(expression):
     for b in expression:
         if b in open_brackets:
             stack.append(b)
-        else if stack.pop() != match_table[b]:
+        elif len(stack) == 0 or match_table[stack.pop()] != b:
             return False
 
-    return True
+    return True if len(stack) == 0 else False
 
 for _ in range(int(input().strip())):
     print("YES" if are_balanced_brackets(input().strip()) else "NO")

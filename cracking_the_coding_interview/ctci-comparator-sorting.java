114ab0d25095e4f6d128ffb55ac2438e249839ea
prudywsh <antoineprudhomme5@gmail.com>
2017-09-04 19:24:31 +0200

    sort array of objects using java comparator

---
+++ cracking_the_coding_interview/ctci-comparator-sorting.java
@@ -0,0 +1,10 @@
+class Checker implements Comparator<Player> {
+
+    public int compare(Player a, Player b) {
+        if (a.score == b.score) {
+            return a.name.compareTo(b.name);
+        }
+        return b.score - a.score;
+    }
+
+}

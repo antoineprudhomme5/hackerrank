9d24037da5c5161a6b7af89b2a323fdfcc688769
prudywsh <antoineprudhomme5@gmail.com>
2017-09-06 17:08:38 +0200

    shortest reach in a graph : simple bfs whithout levels

---
+++ cracking_the_coding_interview/ctci-bfs-shortest-reach.py
@@ -0,0 +1,57 @@
+import queue
+
+class Node:
+    def __init__(self, index):
+        self.neighbors = []
+        self.index = index
+        self.visited = False
+
+class Graph:
+    def __init__(self, size = 0):
+        self.nodes = {}
+        self.size = size
+        self.init_nodes()
+
+    def init_nodes(self):
+        for i in range(1, self.size + 1):
+            self.nodes[i] = Node(i)
+
+    def connect(self, x, y):
+        self.nodes[x].neighbors.append(y)
+        self.nodes[y].neighbors.append(x)
+
+    def find_all_distances(self, s):
+        # init dic with all node as key
+        distances = {}
+        for i in range(self.size):
+            distances[i+1] = -1
+        distances.pop(s, None)
+        # do BFS starting from the given node
+        q = queue.Queue()
+        q.put(s)
+        self.nodes[s].visited = True
+        level = 1
+        while not q.empty():
+            curr = self.nodes[q.get()]
+            if curr.index in distances:
+                distances[curr.index] = 6 * level
+            # enqueue neighbors that are not already visited
+            for neighbor in curr.neighbors:
+                if not self.nodes[neighbor].visited:
+                    q.put(self.nodes[neighbor].index)
+                    self.nodes[neighbor].visited = True
+
+        # return distances
+        return " ".join(list(map(str, distances.values())))
+
+# for each test case
+for _ in range(int(input())):
+    nb_nodes, nb_edges = [int(value) for value in input().split()]
+    # init the graph
+    graph = Graph(nb_nodes)
+    # reach the edges
+    for i in range(nb_edges):
+        x, y = [int(x) for x in input().split()]
+        graph.connect(x, y)
+    s = int(input())
+    print(graph.find_all_distances(s))

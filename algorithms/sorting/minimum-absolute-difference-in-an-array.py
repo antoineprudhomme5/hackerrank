ae2f1e01f3e2d3dc4fe799d03cfb78d0727c635d
prudywsh <antoineprudhomme5@gmail.com>
2017-09-08 12:15:11 +0200

    minimum absolute difference in array

--- algorithms/sorting/minimum-absolute-difference-in-an-array.py
+++ algorithms/sorting/minimum-absolute-difference-in-an-array.py
@@ -6,8 +6,8 @@ v = sorted(list(map(int, input().split())))
 diff = math.inf
 # find the min diff between 2 values
 for i in range(1, len(v)):
-    temp = math.fabs(v[i], v[i-1])
+    temp = math.fabs(v[i] - v[i-1])
     if temp < diff:
         diff = temp
 
-print(diff)
+print(int(diff))

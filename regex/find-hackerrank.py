7e6c8bf103588c0991d0ebe64e8f44863abc3ffa
prudywsh <antoineprudhomme5@gmail.com>
2017-11-08 18:49:21 +0100

    start/end/both by hackerrank

---
+++ regex/find-hackerrank.py
@@ -0,0 +1,11 @@
+import re
+for _ in range(int(input())):
+    s = input().strip()
+    if re.search(r'^hackerrank(.*?hackerrank)?$', s):
+        print('0')
+    elif re.search(r'^hackerrank', s):
+        print('1')
+    elif re.search(r'hackerrank$', s):
+        print('2')
+    else:
+        print('-1')

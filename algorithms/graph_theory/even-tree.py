48f7a7327373d826e2265ed75cf1f4f8c3ba6be3
prudywsh <antoineprudhomme5@gmail.com>
2017-12-27 17:59:32 +0100

    even tree: simpler solution that works perfectly

--- algorithms/graph_theory/even-tree.py
+++ algorithms/graph_theory/even-tree.py
@@ -1,68 +1,19 @@
-import sys
-from collections import deque
-
 class Node(object):
-    def __init__(self, index):
+    def __init__(self):
         self.parent = None
-        self.index = index
-        self.children = []
-        # weight = total number of nodes from this node
         self.weight = 1
 
-class Graph(object):
-    def __init__(self, nb_nodes):
-        self.nodes = {}
-        for i in range(nb_nodes):
-            self.nodes[i+1] = Node(index=i+1)
-
-    def add_edge(self, child, parent):
-        if child < parent:
-            child, parent = parent, child
-        self.nodes[parent].children.append(child)
-        self.nodes[child].parent = parent
-
-    def _find_leafs(self):
-        leafs = []
-        # use bfs to find leafs
-        q = deque()
-        q.append(1)
-        visited = {}
-
-        while len(q):
-            current = self.nodes[q.popleft()]
-            if len(current.children):
-                for child in current.children:
-                    if child not in visited:
-                        visited[child] = True
-                        q.append(child)
-            else:
-                leafs.append(current.index)
-
-        return leafs
-
-    def solve(self):
-        visited = {}
-        leafs_idx = self._find_leafs()
-
-        while len(leafs_idx):
-            new_leafs_idx = []
-            for leaf_idx in leafs_idx:
-                leaf = self.nodes[leaf_idx]
-
-                if leaf.parent:
-                    self.nodes[leaf.parent].weight += leaf.weight
-
-                if leaf.parent and leaf.parent not in visited:
-                    visited[leaf.parent] = True
-                    new_leafs_idx.append(leaf.parent)
-
-            leafs_idx = new_leafs_idx
-
-        return sum(1 for k, v in self.nodes.items() if v.parent != None and v.weight % 2 == 0)
-
 n, m = map(int, input().split())
-g = Graph(n)
+
+nodes = [Node() for _ in range(n)]
 for _ in range(m):
-    i, j = map(int, input().split())
-    g.add_edge(i, j)
-print(g.solve())
+    child, parent = map(int, input().split())
+    child, parent = child-1, parent-1
+    # insert the child and set weights of parents
+    nodes[child].parent = nodes[parent]
+    p = nodes[parent]
+    while p:
+        p.weight += 1
+        p = p.parent
+
+print(sum(1 for node in nodes if node.parent != None and node.weight % 2 == 0))

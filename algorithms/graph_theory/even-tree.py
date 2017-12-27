54eef82bcdfcab4e4f6cfee2a7fe149642e6a162
prudywsh <antoineprudhomme5@gmail.com>
2017-12-27 17:30:34 +0100

    even tree: better solution but only 4/9 test cases

--- algorithms/graph_theory/even-tree.py
+++ algorithms/graph_theory/even-tree.py
@@ -6,8 +6,8 @@ class Node(object):
         self.parent = None
         self.index = index
         self.children = []
-        # weight = total number of children
-        self.weight = 0
+        # weight = total number of nodes from this node
+        self.weight = 1
 
 class Graph(object):
     def __init__(self, nb_nodes):
@@ -20,12 +20,6 @@ class Graph(object):
             child, parent = parent, child
         self.nodes[parent].children.append(child)
         self.nodes[child].parent = parent
-
-    def remove_edge(self, child, parent):
-        if child < parent:
-            child, parent = parent, child
-        self.nodes[parent].children.remove(child)
-        self.nodes[child].parent = None
 
     def _find_leafs(self):
         leafs = []
@@ -48,27 +42,23 @@ class Graph(object):
 
     def solve(self):
         visited = {}
-        nb_subtrees = 0
         leafs_idx = self._find_leafs()
 
         while len(leafs_idx):
-            print(leafs_idx, file=sys.stderr)
             new_leafs_idx = []
             for leaf_idx in leafs_idx:
                 leaf = self.nodes[leaf_idx]
-                if leaf.weight % 2 == 0 and leaf.parent:
-                    self.nodes[leaf.parent].weight += 1
-                else:
-                    print(leaf.index, file=sys.stderr)
-                    nb_subtrees += 1
+
+                if leaf.parent:
+                    self.nodes[leaf.parent].weight += leaf.weight
 
                 if leaf.parent and leaf.parent not in visited:
                     visited[leaf.parent] = True
                     new_leafs_idx.append(leaf.parent)
+
             leafs_idx = new_leafs_idx
 
-        # remove 1 because we want the number of link to remove
-        return nb_subtrees - 1
+        return sum(1 for k, v in self.nodes.items() if v.parent != None and v.weight % 2 == 0)
 
 n, m = map(int, input().split())
 g = Graph(n)

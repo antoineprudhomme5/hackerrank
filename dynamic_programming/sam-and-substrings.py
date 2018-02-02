b375710fb45cc503e6e152068f0efad48ba2b28e
prudywsh <antoineprudhomme5@gmail.com>
2018-02-02 19:03:09 +0100

    dp: sum of all substrings

--- dynamic_programming/sam-and-substrings.py
+++ dynamic_programming/sam-and-substrings.py
@@ -1,17 +1,11 @@
-# 16 : 16 + 1 + 6
-# 123 + 12 + 23 + 1 + 2 + 3
-# 1234 + 123 + 234 + 12 + 23 + 34 + 1 + 2 + 3 + 4
+def solve(s):
+    digits = [int(s[i]) for i in range(len(s))]
+    ans = [0 for _ in range(len(s))]
 
-# 1234
-# 1
-# 2 + 12
-# 3 + 23 + 123 = 3 + 20 + 3 + 120 + 3 = 3*3 + 10(2 + 12)
-# 4 + 34 + 234 + 1234 = 4 + 30 + 4 + 230 + 4 + 1230 + 4 = 4*4 + 10(3 + 23 + 123)
+    ans[0] = digits[0]
+    for i in range(1, len(s)):
+        ans[i] = ((i+1)*digits[i] + 10*ans[i-1]) % (10**9 + 7)
 
-def solve(substring):
-    s = [0]
-    for i in range(len(substring)):
-        s.append(int(substring[i])*(i+1) + 10*s[i])
-    return sum(s)
+    return sum(ans)
 
 print(solve(input()) % (10**9 + 7))

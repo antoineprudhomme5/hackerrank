22ab15d3f0af826fd2dbfa488fdc8167f0082d51
prudywsh <antoineprudhomme5@gmail.com>
2018-07-06 20:56:30 +0200

    fractal trees all

--- shell/fractal_trees_all.sh
+++ shell/fractal_trees_all.sh
@@ -8,48 +8,60 @@ fractal() {
 	height=16
 	indexes=(49)
 
-	while [[ $n > 0 ]];
+	for (( f=0; f<5; f++ ));
 	do
-		n=$((n-1))
-		for (( i=1; i<=$height; i++ ));
-		do
-			t=("${a[@]}")
+		if [[ $n > 0 ]];
+		then
+			n=$((n-1))
+			for (( i=1; i<=$height; i++ ));
+			do
+				t=("${a[@]}")
+				for index in "${indexes[@]}";
+				do
+					t[$index]="1"
+				done
+				echo "${t[@]}"
+			done
+
+			temp_indexes=()
 			for index in "${indexes[@]}";
 			do
-				t[$index]="1"
+				temp_indexes[${#temp_indexes[@]}]=$(($index-1))
+				temp_indexes[${#temp_indexes[@]}]=$(($index+1))
 			done
-			echo "${t[@]}"
-		done
+			indexes=("${temp_indexes[@]}")
 
-		temp_indexes=()
-		for index in "${indexes[@]}";
-		do
-			temp_indexes[${#temp_indexes[@]}]=$(($index-1))
-			temp_indexes[${#temp_indexes[@]}]=$(($index+1))
-		done
-		indexes=("${temp_indexes[@]}")
-
-		for (( i=1; i<=$height; i++ ));
-		do
-			t=("${a[@]}")
-			for index in "${indexes[@]}";
+			for (( i=1; i<=$height; i++ ));
 			do
-				t[$index]="1"
-			done
-			echo "${t[@]}"
+				t=("${a[@]}")
+				for index in "${indexes[@]}";
+				do
+					t[$index]="1"
+				done
+				echo "${t[@]}"
 
-			for (( j=0; j<"${#indexes[@]}"; j++ ))
-			do  
-		   		if [[ $(($j%2)) == 0 ]];
-		   		then
-		   			indexes[$j]=$((${indexes[$j]}-1))
-		   		else
-		   			indexes[$j]=$((${indexes[$j]}+1))
-		   		fi
+				if [[ $i != $height ]];
+				then
+					for (( j=0; j<"${#indexes[@]}"; j++ ))
+					do  
+				   		if [[ $(($j%2)) == 0 ]];
+				   		then
+				   			indexes[$j]=$((${indexes[$j]}-1))
+				   		else
+				   			indexes[$j]=$((${indexes[$j]}+1))
+				   		fi
+					done
+				fi
 			done
-		done
+		else
+			for (( i=0; i<$(($height*2)); i++ ));
+			do
+				echo "${a[@]}"
+			done
+		fi
 		height=$(($height/2))
 	done
+	echo "${a[@]}"
 }
 read n
 fractal $n | tac | tr -d ' '

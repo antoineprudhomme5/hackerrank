ad5189afab330f1d0026c22b79693d6403d79a88
Antoine Prudhomme <antoineprudhomme5@gmail.com>
2019-09-09 08:09:00 +0200

    friend circle queries - almost done

--- interview/misc/friend-circle-queries.py
+++ interview/misc/friend-circle-queries.py
@@ -13,6 +13,9 @@ def max_circle(queries):
   for query in queries:
     a, b = query[0], query[1]
     root_a, root_b = get_root(parents, a), get_root(parents, b)
+    if root_a == root_b:
+      answers.append(current_max_size)
+      continue
 
     size_a = sizes.get(root_a) if root_a in sizes else 1
     size_b = sizes.get(root_b) if root_b in sizes else 1
@@ -28,8 +31,9 @@ def max_circle(queries):
 
   return answers
 
-max_circle([[1, 2], [3, 4], [1, 3], [5, 7], [5, 6], [7, 4]])
+# max_circle([[1, 2], [3, 4], [1, 3], [5, 7], [5, 6], [7, 4]])
 # max_circle([[1, 2], [3, 4], [1, 3]])
+max_circle([[6, 4], [5, 9], [8, 5], [4, 1], [1, 5], [7, 2], [4, 2], [7, 6]])
 
 """
 8
